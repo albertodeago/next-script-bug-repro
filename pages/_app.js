885a1c5d5e2649bcb898db0eb6ec4265555c84b3
Alberto De Agostini <alberto.deagostini@adevinta.com>
2022-01-26 11:12:17 +0100

    add reproduction example

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
+
+{
+  /* <script src="before-interactive.js" defer="" data-nscript="beforeInteractive"></script>
+<script src="after-interactive.js" crossorigin="anonymous" defer="false" data-nscript="afterInteractive"></script> */
+}
---
+++ public/after-interactive.js
@@ -0,0 +1,1 @@
+console.log("after interactive ran!");
